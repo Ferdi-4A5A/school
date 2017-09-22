bc98e1e95b2df345551d03dbd316ad6341e43ef8
Ferdi-4A5A <lolstfu@msn.com>
2017-09-23 00:42:23 +0200

    Add files via upload

---
+++ jaar3/php/victor/5.php
@@ -0,0 +1,14 @@
+<form action="" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if (isset($_POST['submit'])) {
+    $test = move_uploaded_file($_FILES['fileToUpload']['tmp_name'], ''.$_FILES['fileToUpload']['name']);
+    echo '<img src="'.$_FILES['fileToUpload']['name'].'" alt="Mountain View" style="width:304px;height:228px;">';
+
+}
