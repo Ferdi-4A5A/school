90a3b05674ae994560e87275260e2c49cf30e2ec
Ferdi-4A5A <lolstfu@msn.com>
2017-09-23 00:45:04 +0200

    Add files via upload

--- jaar3/php/victor/5.php
+++ jaar3/php/victor/5.php
@@ -4,10 +4,8 @@
     <input type="submit" value="Upload Image" name="submit">
 </form>
 <?php
-ini_set('display_errors', 1);
 
 if (isset($_POST['submit'])) {
     $test = move_uploaded_file($_FILES['fileToUpload']['tmp_name'], '/media/sf_www/school/jaar3/php/victor/'.$_FILES['fileToUpload']['name']);
     echo '<img src="'.$_FILES['fileToUpload']['name'].'" alt="Mountain View" style="width:304px;height:228px;">';
-
 }
